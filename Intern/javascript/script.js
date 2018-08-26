4d64915339251659ab687ca6ff610ab1616f7fe0
praweshlamsal <praweshlamsalk@gmail.com>
2018-08-26 10:04:38 +0545

    Front-end challenge

---
+++ Intern/javascript/script.js
@@ -0,0 +1,20 @@
+var i = 0;
+var txt = 'Are you a frontend ninja?';
+var speed = 70;
+
+function typer() {
+  if (i < txt.length) {
+    document.getElementById("startpage").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typer, speed);
+  }
+}
+
+function closefun(){
+	document.getElementById("check").style.display="none";
+		document.getElementById("btn-close").style.display="none";
+}
+function showfun(){
+	document.getElementById("check").style.display="block";
+		document.getElementById("btn-close").style.display="block";
+}
